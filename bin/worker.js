a2fc13c4bfb59d2ea036e5c803daa87849a7ff8c
Miroslav Bajtoš <oss@bajtos.net>
2025-05-28 14:01:45 +0200

    feat!: subdomain handling
    Change the request URL format to
    
        https://{wallet}.calibration.filcdn.io/{CommP}
    
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- bin/worker.js
+++ bin/worker.js
@@ -12,9 +12,10 @@ export default {
       return new Response('Method Not Allowed', { status: 405 })
     }
 
-    const { proofSetId, pieceCid } = parseRequest(request)
-    if (!proofSetId || !pieceCid) {
-      return new Response('Missing required fields', { status: 400 })
+    const { clientWalletAddress, pieceCid, error } = parseRequest(request, env)
+    console.log({error, pieceCid, clientWalletAddress})
+    if (error || !clientWalletAddress || !pieceCid) {
+      return new Response(error ?? 'Missing required fields', { status: 400 })
     }
 
     // TODO: Record retrieval stats to D1 asynchronously (do not block response)
