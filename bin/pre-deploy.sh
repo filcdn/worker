6eaba70a705b1143f90f4220f1b53c2e1cc93569
Miroslav Bajtoš <oss@bajtos.net>
2025-05-28 16:06:19 +0200

    fix: pre-deploy.sh - PATH to wrangler
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- bin/pre-deploy.sh
+++ bin/pre-deploy.sh
@@ -1,5 +1,7 @@
 #/bin/bash -e
 
+PATH=./node_modules/.bin:$PATH
+
 envsubst < wrangler.toml > wrangler.toml.tmp && mv wrangler.toml.tmp wrangler.toml
 if ! wrangler d1 list | grep -q filcdn-db; then
   wrangler d1 create filcdn-db
