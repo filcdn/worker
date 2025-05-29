0d91d6e169dbbb56c6de92451991b99ce8df79af
Nikolas Haimerl <nikolas@meridian.space>
2025-05-29 10:20:21 +0200

    add performance

--- bin/worker.js
+++ bin/worker.js
@@ -9,6 +9,9 @@ const BASE_URL = 'yablu.net'
 const OWNER_ADDRESS_YABLU = '0x7469b47e006d0660ab92ae560b27a1075eecf97f'
 export default {
   async fetch(request, env, ctx, { retrieveFile = defaultRetrieveFile } = {}) {
+    const performances = {}
+    // Timestamp to measure overall worker execution time
+    const t0 = performance.now()
     if (request.method !== 'GET') {
       return new Response('Method Not Allowed', { status: 405 })
     }
@@ -23,14 +26,19 @@ export default {
       return new Response('Missing required fields', { status: 400 })
     }
 
+    // Timestamp to measure file retrieval performance
+    const t1 = performance.now()
     const response = await retrieveFile(BASE_URL, pieceCid, env.CACHE_TTL)
-    const timestamp = new Date().toISOString()
+    performances.ttfb = performance.now() - t0
+    performances.workerExecution = performance.now() - t1
+
     ctx.waitUntil(
       logRetrievalResult(env, {
         ownerAddress: OWNER_ADDRESS_YABLU,
         clientAddress: clientWalletAddress,
         response,
-        timestamp,
+        timestamp:new Date().toISOString(),
+        performances
       }),
     )
     return response
