1312ae8602dcf918b08663678516bb30a2b7a1c1
Miroslav Bajtoš <oss@bajtos.net>
2025-05-30 17:19:33 +0200

    fix: pre-deploy script
    Add `--env production` when invoking wrangler.
    
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- bin/pre-deploy.sh
+++ bin/pre-deploy.sh
@@ -3,6 +3,6 @@
 PATH=./node_modules/.bin:$PATH
 
 if ! wrangler d1 list | grep -q filcdn-db; then
-  wrangler d1 create filcdn-db
+  wrangler d1 create filcdn-db --env production
 fi
-wrangler d1 migrations apply filcdn-db --remote
+wrangler d1 migrations apply filcdn-db --remote --env production
