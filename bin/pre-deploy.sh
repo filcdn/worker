31fa09276694028ba5b536631396815e1f852b4d
Miroslav Bajtoš <oss@bajtos.net>
2025-05-30 16:26:40 +0200

    fix: --remote for DB migrations executed in CD
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- bin/pre-deploy.sh
+++ bin/pre-deploy.sh
@@ -5,4 +5,4 @@ PATH=./node_modules/.bin:$PATH
 if ! wrangler d1 list | grep -q filcdn-db; then
   wrangler d1 create filcdn-db
 fi
-wrangler d1 migrations apply filcdn-db
+wrangler d1 migrations apply filcdn-db --remote
