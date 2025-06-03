84c43db676020cc77157a8f29b1687ed2cbaef9d
Miroslav Bajtoš <oss@bajtos.net>
2025-06-03 16:48:02 +0200

    fixup! document uint256 representation in SQL
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- db/migrations/0004_add_indexer.sql
+++ db/migrations/0004_add_indexer.sql
@@ -1,3 +1,9 @@
+-- NOTE:
+-- PDPVerifier and Pandora Service contracts use uint256 as ID type
+-- SQLite does not support big integer data types like uint256
+-- We are storing the IDs as TEXT using the "natural" base10 encoding
+-- E.g. the value 0x0...01 is stored as "1"
+
 CREATE TABLE IF NOT EXISTS indexer_proof_sets (
   set_id TEXT PRIMARY KEY,
   owner TEXT NOT NULL
