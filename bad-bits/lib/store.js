80155224749ff836e55e34617b3f9848a2686ee1
Miroslav Bajtoš <oss@bajtos.net>
2025-07-11 15:49:49 +0200

    fixup! small perf opt
    See https://developers.cloudflare.com/d1/worker-api/d1-database/#batch
    
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- bad-bits/lib/store.js
+++ bad-bits/lib/store.js
@@ -8,20 +8,21 @@
 export async function updateBadBitsDatabase(env, currentHashes) {
   try {
     const now = new Date()
+    const stmt = env.DB.prepare(
+      `
+      INSERT INTO bad_bits (hash, last_modified_at) VALUES (?, ?)
+      ON CONFLICT(hash) DO UPDATE SET last_modified_at = excluded.last_modified_at
+      `,
+    )
     await env.DB.batch(
       [
         Array.from(currentHashes).map((hash) =>
-          env.DB.prepare(
-            `
-          INSERT INTO bad_bits (hash, last_modified_at) VALUES (?, ?)
-          ON CONFLICT(hash) DO UPDATE SET last_modified_at = excluded.last_modified_at
-        `,
-          ).bind(hash, now.toISOString()),
+          stmt.bind(hash, now.toISOString()),
         ),
         env.DB.prepare(
           `
-        DELETE FROM bad_bits WHERE last_modified_at < ?
-      `,
+          DELETE FROM bad_bits WHERE last_modified_at < ?
+          `,
         ).bind(now.toISOString()),
       ].flat(),
     )
