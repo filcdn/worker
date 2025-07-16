9fe50b6afac8bc6d69b03b557f5fab1bc2ae04b8
Miroslav Bajtoš <oss@bajtos.net>
2025-07-16 16:07:17 +0200

    REVERT ME: switch to manual invocation
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- bad-bits/bin/scheduler.js
+++ bad-bits/bin/scheduler.js
@@ -1,6 +1,19 @@
 import { fetchAndStoreBadBits } from '../lib/bad-bits.js'
 
 export default {
+  /**
+   * @param {Request} request
+   * @param {Env} env
+   * @param {ExecutionContext} ctx
+   * @returns {Promise<Response>}
+   */
+  async fetch(request, env, ctx) {
+    await this.scheduled(undefined, env, ctx)
+    return new Response('Bad bits updated successfully', {
+      status: 200,
+    })
+  },
+
   async scheduled(_controller, env, _ctx) {
     console.log('Running scheduled bad bits update...')
     try {
