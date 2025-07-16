fd2475b648d4470c833a3436a18782b17741910a
Miroslav Bajtoš <oss@bajtos.net>
2025-07-16 15:48:56 +0200

    fixup! more logs + fix typing error
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- bad-bits/lib/store.js
+++ bad-bits/lib/store.js
@@ -7,6 +7,7 @@
  * @param {string} etag - ETag for the current denylist
  */
 export async function updateBadBitsDatabase(env, currentHashes, etag) {
+  const startedAt = Date.now()
   const now = new Date().toISOString()
   const insertBadBitStmt = env.DB.prepare(
     `
@@ -15,26 +16,29 @@ export async function updateBadBitsDatabase(env, currentHashes, etag) {
       `,
   )
 
+  let updated = 0
   const remainingHashes = Array.from(currentHashes)
   while (remainingHashes.length > 0) {
     // pop first 1000 hashes from remainingHashes
     const batchHashes = remainingHashes.splice(0, 1000)
+    updated += batchHashes.length
     await env.DB.batch(
       batchHashes.map((hash) => insertBadBitStmt.bind(hash, now)),
+    )
+    console.log(
+      `Inserted/updated ${updated} bad bits in ${Date.now() - startedAt}ms`,
     )
   }
 
   const statements = [
-    env.DB.prepare('DELETE FROM bad_bits WHERE last_modified_at < ?').bind(
-      now.toISOString(),
-    ),
+    env.DB.prepare('DELETE FROM bad_bits WHERE last_modified_at < ?').bind(now),
   ]
 
   if (etag) {
     statements.push(
       env.DB.prepare(
         'INSERT INTO bad_bits_history (timestamp, etag) VALUES (?, ?)',
-      ).bind(now.toISOString(), etag),
+      ).bind(now, etag),
     )
   }
 
