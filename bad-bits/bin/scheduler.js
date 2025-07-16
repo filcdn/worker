9111c4846cb3fe44e10a974793f219d656ebe519
Miroslav Bajtoš <oss@bajtos.net>
2025-07-16 08:41:43 +0200

    Apply suggestion from @juliangruber
    Co-authored-by: Julian Gruber <julian@juliangruber.com>

--- bad-bits/bin/scheduler.js
+++ bad-bits/bin/scheduler.js
@@ -2,7 +2,7 @@ import { fetchAndStoreBadBits } from '../lib/bad-bits.js'
 
 export default {
   async scheduled(_controller, env, _ctx) {
-    console.log('Running scheduled badbits update...')
+    console.log('Running scheduled bad bits update...')
     try {
       await fetchAndStoreBadBits(env)
       console.log('Updated badbits denylist')
