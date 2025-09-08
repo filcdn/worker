7b4681e98cbf37723bf2f21f98a5ff9a4f283582
Srdjan S <stankovic.srdjo@gmail.com>
2025-09-08 14:00:22 +0200

    Clean terminate_service_tx_hash when transaction is executed

--- terminator/lib/transaction-monitor-workflow.js
+++ terminator/lib/transaction-monitor-workflow.js
@@ -33,6 +33,14 @@ export class TransactionMonitorWorkflow extends WorkflowEntrypoint {
           })
         },
       )
+
+      await step.do('clean when transaction confirmed', {}, async () => {
+        this.env.DB.prepare(
+          'UPDATE data_sets SET terminate_service_tx_hash = NULL WHERE terminate_service_tx_hash = ?',
+        )
+          .bind(transactionHash)
+          .run()
+      })
     } catch (error) {
       // Send message to cancel queue
       await step.do(
