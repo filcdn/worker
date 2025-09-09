60861d2c6e92819c9cbe1b357f01eb1097fa57d3
Miroslav Bajtoš <oss@bajtos.net>
2025-09-09 10:45:52 +0200

    fixup! derive DB from ENV
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- db/deploy-calibration.sh
+++ db/deploy-calibration.sh
@@ -6,8 +6,8 @@ cd "$(dirname "$(readlink -f "$0")")"
 # Add monorepo's node_modules to the PATH
 PATH=../node_modules/.bin:$PATH
 
-DB=filcdn-calibration-db
 ENV=calibration
+DB=filcdn-$ENV-db
 
 if ! wrangler d1 list | grep -q "$DB"; then
   wrangler d1 create "$DB" --env "$ENV"
